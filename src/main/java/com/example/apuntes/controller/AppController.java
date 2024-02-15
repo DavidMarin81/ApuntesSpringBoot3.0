ea7f3d79a58e3d5b87a47157dc4a9098bfe6a253
DavidMarin81 <davidmaringomez81@gmail.com>
2024-02-15 18:51:40 +0100

    Navegando hacia el index.html

--- src/main/java/com/example/apuntes/controller/AppController.java
+++ src/main/java/com/example/apuntes/controller/AppController.java
@@ -3,19 +3,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /*
- Navegando de una pagina a otra
- 1.- Se anota la clase con @Controller
- 2.- Se anota el metodo con GetMapping y se indica la ruta a donde navegar
- 3.- El return hace referencia a la pagina .html
- 4.- Se crea un archivo formulario.html en /templates
- 5.- En el index, se pone un <a href> para navegar a formulario.html
+ Volviendo al index.html desde formulario.html
+ 1.- Se crea un metodo que regrese al index.html
+
+ - El atributo del GetMapping hace referencia al href="" del .html
  */
 
 @Controller
 public class AppController {
 
-    @GetMapping("/formulario.html")
+    @GetMapping("/formulario")
     public String mostrarFormulario() {
         return "formulario";
+    }
+
+    @GetMapping("/index")
+    public String mostrarIndex() {
+        return "redirect:/index.html";
     }
 }
